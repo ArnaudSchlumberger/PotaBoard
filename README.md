a7d9a777030479ba81c1e64b9d6cc27388f08cac
ArnaudSchlumberger <arn.schlumberger@gmail.com>
2021-07-19 11:53:44 +0200

    Retrait fonctionnalite DUTY CYCLE sur les DI - Mise a jour valeur de resistance AOx

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 This repository contains all the code, and PCB design files related to PotaBoard, a BLE-powered board kitchen garden.
 
+
 It comes with capacitive-moisure sensor and BME280 compatibility for temperature and pressure measurements.
+
+Used microcontroller : ATtiny1604 with Arduino Nano UPDI Programmer
+
+
 BLE module : EMB1061.
 
 All eletronics-related files are in 'Electronics - Extras' folder, and contains:
--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,34 @@
 #include "BME.h"
 #include "PotaData.h"
 
+#include "clock_management.h"
 
 #define DELAY_BEFORE_SLEEP 50 //ms; delay at end of loop
 #define EMB1061_TRANSPARENT_DELAY 1000 // ms; delay allowing BLE to leave Boot Mode and enter Transparent Mode
 float temperature;
 float pression;
+int8_t osc_err;
+bool bmp_ok;
 
 Adafruit_BMP280 bmp;
 
 void setup() {
-  Serial.begin(115200);
+
+  Serial.begin(7680); //9600 adjusted baud (for this chip, clock freq is multiplied by 1.25)
+  delay(100);
+  /* 
+  Serial.print("**\r");
+  Serial.print("AT+BNAME=POTABOARD\r");
+  delay(2000);
+  Serial.print("AT+UARTCFG=9600,8,0,1,0\r");
+  delay(2000);
+  Serial.print("AT+SAVE\r");
+  Serial.print("AT+REBOOT\r");
+  */
   delay(EMB1061_TRANSPARENT_DELAY); //Waits for BLE Module to enter Transparent Mode (UART Direct Transmission Mode)
+
   Wire.begin();
-  bmp.begin(BME280_ADDR,BME280_ID_VALUE);
+  bmp_ok = bmp.begin(BME280_ADDR,BME280_ID_VALUE);
   bmp.setSampling(Adafruit_BMP280::MODE_FORCED,
                   Adafruit_BMP280::SAMPLING_X4,
                   Adafruit_BMP280::SAMPLING_X4,
@@ -34,16 +49,23 @@ void setup() {
 }
 
 void loop(){
-  while(Serial.available()>0){
-    if(String(Serial.peek())=="232"){
+  while(Serial.available()>0 and bmp_ok){
+    if((Serial.peek())==0x0A){
       Serial.read();
       collectSendDataTP(bmp, temperature,pression);
     }
-    else if(String(Serial.peek())=="161"){
+    else if(Serial.peek()==0x0E){
       Serial.read();
       collectSendDataH();
     }
+    else{
+        Serial.write(Serial.read()); //Permet d'adapter les lignes précédentes
+    }
+  }
+  if(!bmp_ok){
+    Serial.write("BMP ERR");
   }
   delay(DELAY_BEFORE_SLEEP);
   sleep();
+  delay(500);
 }
